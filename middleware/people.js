75dcdb6c2e00e2806069930643fde5409e4aade5
Sanchit Mehta <sanckumar@paypal.com>
2015-11-28 20:45:36 +0530

    Update people

--- middleware/people.js
+++ middleware/people.js
@@ -2,10 +2,32 @@
 var people = require('../model/people');
 var uuid = require('node-uuid');
 
+function getKey(x){
+	var matches = [];
+
+	var pattern = /\[(.*?)\]/g;
+	var match;
+	while ((match = pattern.exec(x)) != null)
+	{
+	  matches.push(match[1]);
+	}
+	return matches[0];
+}
+
+function getField(x){
+	var matches = [];
+
+	var pattern = /\[(.*?)\]/g;
+	var match;
+	while ((match = pattern.exec(x)) != null)
+	{
+	  matches.push(match[1]);
+	}
+	return matches[1];
+}
 
 exports.addNewPersonInformation = function(req, res){
 
-	console.log(req.body);
 	var personName = req.body.personName || null;
 	var personPhone = req.body.personPhone || null;
 	var immediateFamilyName = req.body.immediateFamilyName || null;
@@ -61,7 +83,69 @@ exports.addNewPersonInformation = function(req, res){
 };
 
 exports.edit = function(req, res){
+	console.log(req.body);
+	/*var body = req.body;
+	delete body.action;
+
+	var updateData = {};
+	for (var key in body) {
+		var id = getKey(key);
+		var fieldName = getField(key);
+		if(fieldName == "personName"){
+
+		}
+		console.log(id + " " + fieldName);
+		updateData[fieldName] = body[key];
+	}*/
+
+	var personName = req.body.personName || null;
+	var personPhone = req.body.personPhone || null;
+	var immediateFamilyName = req.body.immediateFamilyName || null;
+	var immediateFamilyPhone = req.body.immediateFamilyPhone || null;
+	var entryDate = req.body.entryDate || null;
+	var exitDate = req.body.exitDate || null;
+	var bucket = req.body.bucket || null;
+	var languagePreference = req.body.languagePreference || null;
+	var id = uuid.v1();
+
+	if(personName == null || personPhone == null){
+		console.log("Incomplete patient information\n");
+		res.send({success: false, msg: "Incomplete patient information"});
+		return;
+	}
+
+	if(immediateFamilyName == null || immediateFamilyPhone == null){
+		console.log("Incomplete support person information\n");
+		res.send({success: false, msg: "Incomplete support person information"});
+		return;
+	}
+
+	var updateData = {
+		id : id,
+		person : {
+			name : personName,
+			phone : personPhone
+		},
+		immediateFamily : {
+			name : immediateFamilyName,
+			phone: immediateFamilyPhone
+		},
+		entryDate : entryDate,
+		exitData : exitDate,
+		bucket : bucket,
+		languagePreference : languagePreference,
+		failedContactCount : failedContactCount
+	};
 
 
+	people.findOneAndUpdate({id: id}, updateData, {upsert:true}, function(err, doc){
+	    if (err){
+	    	console.log(err);
+	    	res.send({success: false, msg: "update query failed"});
+	    	return;
+	    } 
+	    res.send("succesfully saved");
+	});
+	console.log(updateData);
 
 }
